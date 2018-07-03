55f444004473b8bce52efa5042d1157ffc440efd
Shilin Ramrakhyani <shils2030@gmail.com>
2018-07-03 14:22:14 +0100

    Made a list with tuples and a test to check for the functions and add to to the counter.
    Refs #10

--- validator/test_system.py
+++ validator/test_system.py
@@ -16,7 +16,7 @@ class TestInstanceCreated(unittest.TestCase):
 
     def test_list_is_correct(self):
         instrument_controls = InstrumentControls()
-        expected_result = ["print_value", "add", "wait"]
+        expected_result = [("print_value", [""]), ("add", [1, 1]), ("wait", [0])]
         instrument_controls.print_value("")
         instrument_controls.add(1, 1)
         instrument_controls.wait(0)
@@ -24,21 +24,33 @@ class TestInstanceCreated(unittest.TestCase):
 
     def test_function_is_called(self):
         instrument_controls = InstrumentControls()
-        instrument_controls.print_value("")
-        instrument_controls.print_value("")
-        instrument_controls.print_value("")
-        self.assertEqual(instrument_controls.track_function.count("print_value"), 3)
+        instrument_controls.print_value("Hello")
+        instrument_controls.print_value("World")
+        instrument_controls.print_value("Python")
+        counter = 0
+        for function_tuple in instrument_controls.track_function:
+            if function_tuple[0] == "print_value":
+                counter = counter + 1
+        self.assertEqual(counter, 3)
 
     def test_add_list_works(self):
         instrument_controls = InstrumentControls()
         instrument_controls.add(1, 2)
         instrument_controls.add(3, 4)
         instrument_controls.add(5, 6)
-        self.assertEqual(instrument_controls.track_function.count("add"), 3)
+        counter = 0
+        for function_tuple in instrument_controls.track_function:
+            if function_tuple[0] == "add":
+                counter = counter + 1
+        self.assertEqual(counter, 3)
 
     def test_wait_list_works(self):
         instrument_controls = InstrumentControls()
         instrument_controls.wait(0)
-        instrument_controls.wait(0)
-        instrument_controls.wait(0)
-        self.assertEqual(instrument_controls.track_function.count("wait"), 3)
+        instrument_controls.wait(1)
+        instrument_controls.wait(2)
+        counter = 0
+        for function_tuple in instrument_controls.track_function:
+            if function_tuple[0] == "wait":
+                counter = counter + 1
+        self.assertEqual(counter, 3)
