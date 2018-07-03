79492b104f65166bb3472c5a11d93c5217c9155d
Shilin Ramrakhyani <shils2030@gmail.com>
2018-07-03 10:54:00 +0100

    Added a list that keeps track of the functions.
    Refs #7

--- validator/instrument_controls.py
+++ validator/instrument_controls.py
@@ -4,19 +4,15 @@ from validator.instrument import Instrument
 
 class InstrumentControls(object):
     new_instrument = None
-    print_counter = 0
-    add_counter = 0
-    wait_counter = 0
+    track_function = []
 
     def __init__(self):
         self.new_instrument = Instrument()
-        self.print_counter = 0
-        self.add_counter = 0
-        self.wait_counter = 0
+        self.track_function = []
 
     def print_value(self, value_to_print):
         print(value_to_print)
-        self.print_counter = self.print_counter + 1
+        self.track_function.append("print_value")
 
     def add(self, first, second):
         try:
@@ -24,7 +20,7 @@ class InstrumentControls(object):
             int(second)
         except ValueError:
             raise ValueError("Not a number")
-        self.add_counter = self.add_counter + 1
+        self.track_function.append("add")
         ans = first + second
         return ans
 
@@ -34,4 +30,4 @@ class InstrumentControls(object):
         except ValueError:
             raise ValueError("Not a number")
         time.sleep(sleep_timer)
-        self.wait_counter = self.wait_counter + 1
+        self.track_function.append("wait")
--- validator/test_system.py
+++ validator/test_system.py
@@ -14,23 +14,31 @@ class TestInstanceCreated(unittest.TestCase):
         new_instrument = Instrument()
         self.assertIsNotNone(new_instrument.new_sample)
 
+    def test_list_is_correct(self):
+        instrument_controls = InstrumentControls()
+        expected_result = ["print_value", "add", "wait"]
+        instrument_controls.print_value("")
+        instrument_controls.add(1, 1)
+        instrument_controls.wait(0)
+        self.assertListEqual(instrument_controls.track_function, expected_result)
+
     def test_function_is_called(self):
         instrument_controls = InstrumentControls()
         instrument_controls.print_value("")
         instrument_controls.print_value("")
         instrument_controls.print_value("")
-        self.assertEqual(instrument_controls.print_counter, 3)
+        self.assertEqual(instrument_controls.track_function.count("print_value"), 3)
 
-    def test_add_counter_works(self):
+    def test_add_list_works(self):
         instrument_controls = InstrumentControls()
         instrument_controls.add(1, 2)
         instrument_controls.add(3, 4)
         instrument_controls.add(5, 6)
-        self.assertEqual(instrument_controls.add_counter, 3)
+        self.assertEqual(instrument_controls.track_function.count("add"), 3)
 
-    def test_wait_counter_works(self):
+    def test_wait_list_works(self):
         instrument_controls = InstrumentControls()
-        instrument_controls.wait(1)
-        instrument_controls.wait(2)
-        instrument_controls.wait(3)
-        self.assertEqual(instrument_controls.wait_counter, 3)
+        instrument_controls.wait(0)
+        instrument_controls.wait(0)
+        instrument_controls.wait(0)
+        self.assertEqual(instrument_controls.track_function.count("wait"), 3)
