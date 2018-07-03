b3e8522263493b24b6e6e0a66ce032eb023bbeea
Shilin Ramrakhyani <shils2030@gmail.com>
2018-07-03 09:37:23 +0100

    Added a counter to all the functions and a test to see if it works.
    Refs #6

--- validator/instrument_controls.py
+++ validator/instrument_controls.py
@@ -4,28 +4,34 @@ from validator.instrument import Instrument
 
 class InstrumentControls(object):
     new_instrument = None
+    print_counter = 0
+    add_counter = 0
+    wait_counter = 0
 
     def __init__(self):
         self.new_instrument = Instrument()
+        self.print_counter = 0
+        self.add_counter = 0
+        self.wait_counter = 0
 
-    @staticmethod
-    def print_value(value_to_print):
+    def print_value(self, value_to_print):
         print(value_to_print)
+        self.print_counter = self.print_counter + 1
 
-    @staticmethod
-    def add(first, second):
+    def add(self, first, second):
         try:
             int(first)
             int(second)
         except ValueError:
             raise ValueError("Not a number")
+        self.add_counter = self.add_counter + 1
         ans = first + second
         return ans
 
-    @staticmethod
-    def wait(sleep_timer):
+    def wait(self, sleep_timer):
         try:
             int(sleep_timer)
         except ValueError:
             raise ValueError("Not a number")
         time.sleep(sleep_timer)
+        self.wait_counter = self.wait_counter + 1
--- validator/test_system.py
+++ validator/test_system.py
@@ -13,3 +13,24 @@ class TestInstanceCreated(unittest.TestCase):
     def test_sample_no_longer_none(self):
         new_instrument = Instrument()
         self.assertIsNotNone(new_instrument.new_sample)
+
+    def test_function_is_called(self):
+        instrument_controls = InstrumentControls()
+        instrument_controls.print_value("")
+        instrument_controls.print_value("")
+        instrument_controls.print_value("")
+        self.assertEqual(instrument_controls.print_counter, 3)
+
+    def test_add_counter_works(self):
+        instrument_controls = InstrumentControls()
+        instrument_controls.add(1, 2)
+        instrument_controls.add(3, 4)
+        instrument_controls.add(5, 6)
+        self.assertEqual(instrument_controls.add_counter, 3)
+
+    def test_wait_counter_works(self):
+        instrument_controls = InstrumentControls()
+        instrument_controls.wait(1)
+        instrument_controls.wait(2)
+        instrument_controls.wait(3)
+        self.assertEqual(instrument_controls.wait_counter, 3)
