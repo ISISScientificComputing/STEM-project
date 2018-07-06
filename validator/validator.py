b8078f706e776311e58cf24ccd45636cda828ff3
Shilin Ramrakhyani <shils2030@gmail.com>
2018-07-06 10:07:20 +0100

    Created a validator with testing for lists
    Refs #13

--- validator/validator.py
+++ validator/validator.py
@@ -1,23 +1,26 @@
 class Validator(object):
     expected_functions = []
     user_functions = []
-    different_functions = []
+    hints_functions = []
     points = 0
     total = 0
     right_percentage = 0
 
     def __init__(self, input_list):
         self.expected_functions = input_list
+        self.hints_functions = []
+        self.user_functions = []
+        self.points = 0
+        self.total = 0
+        self.right_percentage = 0
 
     def execute_user_input(self):
         from validator.user_script import IC
         self.user_functions = IC.track_function
         if len(self.user_functions) == 0:
-            print("User Script is empty")
-            exit()
-        else:
-            print("it worked")
-        #print(IC.track_function)
+            raise IOError("No user functions found")
+
+        # Identify short list
 
         max_iterations = len(self.expected_functions)
         min_iterations = len(self.user_functions)
@@ -33,9 +36,15 @@ class Validator(object):
                 if self.expected_functions[index] == self.user_functions[index]:
                     self.points = self.points + 10
                 else:
-                    print("Hello World")
-            self.right_percentage = self.points/self.total
-        print("Percentage of right code: {}%".format(self.right_percentage*100))
-
-
-
+                    if "set_sample_position" == self.user_functions[index][0]:
+                        self.hints_functions.append("Your sample positions is not quite right")
+                    elif "collect_data" == self.user_functions[index][0]:
+                        self.hints_functions.append("Your wait time is not quite right")
+                    elif "set_rotation" == self.user_functions[index][0]:
+                        self.hints_functions.append("Your sample rotation is not quite right")
+            else:
+                if max_iterations == len(self.expected_functions):
+                    self.hints_functions.append("You are missing this function: " + self.expected_functions[index][0])
+                else:
+                    self.hints_functions.append("You have too many of this function: " + self.user_functions[index][0])
+        self.right_percentage = (self.points/self.total)*100
