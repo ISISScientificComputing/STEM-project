4ae78be84909f75f41672a2315c3c67eb70c1c11
Shilin Ramrakhyani <shils2030@gmail.com>
2018-07-16 15:31:46 +0100

    clean up expected functions
    refs  #20

---
+++ stem_project/validator/function_wrapper.py
@@ -0,0 +1,10 @@
+class FunctionWrapper (object):
+    name_of_function = None
+    list_of_arguements = None
+
+    def __init__(self, name, arguements):
+        try:
+            self.name_of_function = str(name)
+        except ValueError:
+            raise ValueError("Not a string")
+        self.list_of_arguements  = arguements
--- stem_project/validator/tests/test_validator.py
+++ stem_project/validator/tests/test_validator.py
@@ -1,40 +1,65 @@
 import unittest
 
 from stem_project.validator.validator import Validator
+from stem_project.validator.function_wrapper import FunctionWrapper
 
 
 class TestValidator(unittest.TestCase):
 
     def test_expected_equal_to_actual(self):
-        validator = Validator([("set_sample_position", [1, 1, 1]), ("set_sample_rotation", [0]),
-                               ("collect_data", [0]), ("set_sample_rotation", [120]),
-                               ("collect_data", [0]), ("set_sample_rotation", [240]),
-                               ("collect_data", [0]), ("set_sample_rotation", [360])])
+        set_sample_position_1_1_1 = FunctionWrapper("set_sample_position", [1, 1, 1])
+        collect_data_0 = FunctionWrapper("collect_data", [0])
+        set_sample_rotation_0 = FunctionWrapper("set_sample_rotation", [0])
+        set_sample_rotation_120 = FunctionWrapper("set_sample_rotation", [120])
+        set_sample_rotation_240 = FunctionWrapper("set_sample_rotation", [240])
+        set_sample_rotation_360 = FunctionWrapper("set_sample_rotation", [360])
+        validator = Validator([set_sample_position_1_1_1, set_sample_rotation_0,
+                               collect_data_0, set_sample_rotation_120,
+                               collect_data_0, set_sample_rotation_240,
+                               collect_data_0, set_sample_rotation_360])
         validator.execute_user_input()
         self.assertEqual(validator.right_percentage, 100)
         self.assertEqual(len(validator.hints_functions), 0)
 
     def test_expected_more_than_actual(self):
-        validator = Validator([("set_sample_position", [1, 1, 1]), ("set_sample_rotation", [0]),
-                               ("collect_data", [0]), ("set_sample_rotation", [120]),
-                               ("collect_data", [0]), ("set_sample_rotation", [240]),
-                               ("collect_data", [0]), ("set_sample_rotation", [360]),
-                               ("launch nuke", [3, 2, 1])])
+        set_sample_position_1_1_1 = FunctionWrapper("set_sample_position", [1, 1, 1])
+        collect_data_0 = FunctionWrapper("collect_data", [0])
+        set_sample_rotation_0 = FunctionWrapper("set_sample_rotation", [0])
+        set_sample_rotation_120 = FunctionWrapper("set_sample_rotation", [120])
+        set_sample_rotation_240 = FunctionWrapper("set_sample_rotation", [240])
+        set_sample_rotation_360 = FunctionWrapper("set_sample_rotation", [360])
+        launch_nuke_3_2_1 = FunctionWrapper("launch nuke", [3, 2, 1])
+        validator = Validator([set_sample_position_1_1_1, set_sample_rotation_0,
+                               collect_data_0, set_sample_rotation_120,
+                               collect_data_0, set_sample_rotation_240,
+                               collect_data_0, set_sample_rotation_360,
+                               launch_nuke_3_2_1])
         validator.execute_user_input()
         self.assertEqual(validator.right_percentage, 88.88888888888889)
         self.assertEqual(len(validator.hints_functions), 1)
 
     def test_expected_less_than_actual(self):
-        validator = Validator([("set_sample_position", [1, 1, 1]), ("set_sample_rotation", [0]), ("collect_data", [0])])
+        set_sample_position_1_1_1 = FunctionWrapper("set_sample_position", [1, 1, 1])
+        set_sample_rotation_0 = FunctionWrapper("set_sample_rotation", [0])
+        collect_data_0 = FunctionWrapper("collect_data", [0])
+        validator = Validator([set_sample_position_1_1_1, set_sample_rotation_0, collect_data_0])
         validator.execute_user_input()
         self.assertEqual(validator.right_percentage, 37.5)
         self.assertEqual(len(validator.hints_functions), 5)
 
     def test_expected_different_arguments_actual(self):
-        validator = Validator([("set_sample_position", [1, 2, 3]), ("set_sample_rotation", [6]),
-                               ("collect_data", [9]), ("set_sample_rotation", [420]),
-                               ("collect_data", [420]), ("set_sample_rotation", [9000]),
-                               ("collect_data", [24]), ("set_sample_rotation", [7])])
+        set_sample_position_1_2_3 = FunctionWrapper("set_sample_position", [1, 2, 3])
+        set_sample_rotation_6 = FunctionWrapper("set_sample_rotation", [6])
+        set_sample_rotation_420 = FunctionWrapper("set_sample_rotation", [420])
+        set_sample_rotation_9000 = FunctionWrapper("set_sample_rotation", [9000])
+        set_sample_rotation_7 = FunctionWrapper("set_sample_rotation", [7])
+        collect_data_9 = FunctionWrapper("collect_data", [9])
+        collect_data_420 = FunctionWrapper("collect_data", [420])
+        collect_data_24 = FunctionWrapper("collect_data", [24])
+        validator = Validator([set_sample_position_1_2_3, set_sample_rotation_6,
+                               collect_data_9, set_sample_rotation_420,
+                               collect_data_420, set_sample_rotation_9000,
+                               collect_data_24, set_sample_rotation_7])
         validator.execute_user_input()
         self.assertEqual(validator.right_percentage, 0)
         self.assertEqual(len(validator.hints_functions), 8)
